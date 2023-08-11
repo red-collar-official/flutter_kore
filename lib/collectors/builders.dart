7d5a5ffcbc0e696793c87235c5ec3b1c98d91f0f
efanov <efanov@redcollar.ru>
2023-08-12 01:59:04 +0300

    Fix generators order

--- lib/collectors/builders.dart
+++ lib/collectors/builders.dart
@@ -2,8 +2,8 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'collectors.dart';
 
-Builder generateInstanceCollector(BuilderOptions options) => SharedPartBuilder(
+Builder generateInstanceCollector(BuilderOptions options) => PartBuilder(
       [InstancesCollectorGenerator()],
-      'instances_collector_generator',
+      '.temp.dart',
       allowSyntaxErrors: true,
     );
