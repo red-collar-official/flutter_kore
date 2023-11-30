98a78bd8a64a149f70292adba2fe9c46b1c2cc88
efanov <efanov@redcollar.ru>
2023-12-01 00:38:53 +0300

    Change coverage file location

--- packages/umvvm/coverage.sh
+++ packages/umvvm/coverage.sh
@@ -1,4 +1,3 @@
-cd ./packages/umvvm
 flutter test --coverage
 lcov --remove coverage/lcov.info "lib/ui/widgets/*" "lib/domain/data/*" "lib/resources/*" "lib/plugins/*" "lib/ui/**/components/*" "lib/ui/**/pages/*" "lib/ui/dialogs/*" "lib/domain/global/*" "lib/domain/interactors/navigation/*" "lib/ui/bottom_sheets/*" "lib/ui/**/utility/*" "lib/**/*.g.dart" "lib/**/*_state.dart" "lib/**/*_view.dart" -o coverage/fixed_lcov.info
 genhtml coverage/fixed_lcov.info -o coverage/html
